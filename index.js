91d477211dc4d0dec621e41b51d0f4cfa82f8b10
AlanDeveloper <alanssantos32@gmail.com>
2022-07-24 19:12:22 -0300

    Init project Calendar

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import expressEjsLayouts from "express-ejs-layouts";
+import http from "http";
+import routes from "./src/routes/index.js";
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(expressEjsLayouts);
+app.set("layout extractScripts", true)
+
+app.use(express.static("public"));
+app.use("/", routes);
+
+app.set("view engine", "ejs");
+app.set("views", "src/views");
+
+server.listen(3000, () => {
+    console.log("listening on *:3000");
+});
