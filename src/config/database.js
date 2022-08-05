7bb104740d85a0dc4fb7830ee43c2e5c9a3167d7
AlanDeveloper <alanssantos32@gmail.com>
2022-08-05 11:43:52 -0300

    Update ssl

--- src/config/database.js
+++ src/config/database.js
@@ -36,7 +36,7 @@ const db = pgp({
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
-    ssl: process.env.APP_ENV == "production" ? true : false
+    ssl: process.env.APP_ENV == "production" ? { rejectUnauthorized: false } : false
 });
 
 export default db;
